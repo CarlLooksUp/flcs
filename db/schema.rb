8d8bbb1badf128799ed0dabe7272023868b9f995
Will Richard <will@tumblr.com>
2014-06-30 10:01:12 -0400

    Added tier column to players table
    Eventually, we'll probably want different tiers, but this is a good place to start I think

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140622222438) do
+ActiveRecord::Schema.define(version: 20140630135800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140622222438) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "team_id"
+    t.integer  "tier"
   end
 
   create_table "season_totals", force: true do |t|
